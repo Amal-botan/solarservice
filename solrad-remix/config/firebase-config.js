bf2507b1b6f96c08b43fc878db63c16720898c88
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-03-27 01:55:51 -0400

    Create firebase-config.js

---
+++ solrad-remix/config/firebase-config.js
@@ -0,0 +1,19 @@
+const config = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
+  databaseURL: process.env.REACT_APP_databaseURL,
+};
+
+export function getFirebaseConfig() {
+  if (!config || !config.apiKey) {
+    throw new Error('No Firebase configuration object provided.' + '\n' +
+    'Add your web app\'s configuration object to firebase-config.js');
+  } else {
+    return config;
+  }
+}
