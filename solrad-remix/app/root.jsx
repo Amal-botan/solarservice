651193804ffd6c282924797e486f1fe8f4861a6d
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-03-27 20:29:44 -0400

    add tailwind to root

--- solrad-remix/app/root.jsx
+++ solrad-remix/app/root.jsx
@@ -6,13 +6,18 @@ import {
   Scripts,
   ScrollRestoration,
 } from "remix";
+import styles from "./styles/app.css"
 
 export function meta() {
   return {
     charset: "utf-8",
-    title: "New Remix App",
+    title: "Square Solar App",
     viewport: "width=device-width,initial-scale=1",
   };
+}
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }]
 }
 
 export default function App() {
