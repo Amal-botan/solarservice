a2f8cf31486c3c6078c82ad83d5b97ca2b44e388
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-03-28 17:17:49 -0400

    Update HeaderNav.jsx

--- solrad-remix/app/HeaderNav.jsx
+++ solrad-remix/app/HeaderNav.jsx
@@ -1,32 +1,76 @@
-import { useTransition } from 'remix';
+import { useTransition, Link } from 'remix';
 
 export default function HeaderNav () {
   // show current route with different title or make it an image style it differently
   const transition = useTransition()
   return (
     <nav className="header header-nav">
+    <img src={siteLogo} alt={altTextLogo} />
+    <span className="header-nav span-nav">
+      {" "}
+      <p className="span-nav nav-title">
+        <Link to="/">Home</Link>
+      </p>
+      <img
+        className="span-nav nav-img"
+        src={homeIcon}
+        alt={homeIconAltText}
+      />
+    </span>{" "}
+    <span className="header-nav span-nav">
+      <p className="span-nav nav-title">
+        <Link to="projects">Projects</Link>
+      </p>
+      <img
+        className="span-nav nav-img"
+        src={projectIcon}
+        alt={projectIconAltText}
+      />
+    </span>
+    <span className="header-nav span-nav">
+      <p className="span-nav nav-title">
+        <Link to="chat">Chat</Link>
+      </p>
+      <img
+        className="span-nav nav-img"
+        src={chatIcon}
+        alt={chatIconAltText}
+      />
+    </span>
+    <Form method="post">
+      <label className="header-nav nav-label">
+        Search: <img src={searchIcon} alt={searchIconAltText} />
+        <input
+          className="header-nav nav-input"
+          type="text"
+          name="search"
+        />
+      </label>
+      {transition.submission ? (
+        <span className="header-nav span-loader">
           <img
-            src={siteLogo}
-            alt={altTextLogo}
+            className="span-nav nav-img"
+            src={loadingIcon}
+            alt={loadingIconAltText}
           />
-          <p className="header-nav nav-title">Home</p>
-          <p className="header-nav nav-title">Projects</p>
-          <p className="header-nav nav-title">Chat</p>
-          <Form method="post">
-            <label className="header-nav nav-label">
-              Search:{" "}
-              <input
-                className="header-nav nav-input"
-                type="submit"
-                name="search"
-              />
-            </label>
-            {transition.submission ? (
-              <span className="header-nav nav-loader">
-                <svg></svg>
-              </span>
-            ) : null}
-          </Form>
-        </nav>
+        </span>
+      ) : null}
+    </Form>
+    {userLoggedIn ? (
+      <div className="header-nav div-nav">
+        <p className="header-nav div-nav" >
+          <Link to="logout">logout</Link>
+        </p>
+        <img className="div-nav div-img" src={logoutIcon} alt={logoutIconAltText} />
+      </div>
+    ) : (
+      <div className="header-nav div-nav">
+        <p className="div-nav nav-text">
+          <Link to="logout">logout</Link>
+        </p>
+        <img className="div-nav nav-img" src={loginIcon} alt={loginIconAltText} />
+      </div>
+    )}
+  </nav>
   )
 }
